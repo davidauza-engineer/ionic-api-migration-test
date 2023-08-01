0c143d00d0700d8ecd0a6316a9dfff95a53229c5
David Auza <davidauza01@gmail.com>
2023-08-01 08:56:42 -0500

    Update graphql-db.js

---
+++ graphql-db.js
@@ -0,0 +1,14 @@
+module.exports = {
+  posts: [
+    {
+      "id": 1,
+      "title": "graphql-json-server",
+      "author": "typicode"
+    },
+    {
+      "id": 2,
+      "title": "graphql-json-server-2",
+      "author": "typicode-2"
+    }
+  ]
+}
