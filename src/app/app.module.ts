fd3a2dd09f96eeb0d38a6a52e920852c40e8bec8
David Auza <davidauza01@gmail.com>
2023-08-01 10:42:38 -0500

    Install apollo-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, GraphQLModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/constants/api.constants.ts
+++ src/app/constants/api.constants.ts
@@ -1,3 +1,3 @@
 // These should be env variables
 export const REST_API_BASE_URL = 'http://localhost:3000';
-export const GRAPHQL_API_BASE_URL = 'http://localhost:3001';
+export const GRAPHQL_API_BASE_URL = 'http://localhost:3001/graphql';
